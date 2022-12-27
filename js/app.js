ada001e2ad11d252cdc2aa0c324aeda35849ceae
ebeauty75 <63450158+ebeauty75@users.noreply.github.com>
2022-12-26 20:33:58 -0800

    Added new files

---
+++ js/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+// Hero Slider
+	$('#hero-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    items:1,
+    dots:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    navText:['PREV', 'NEXT'],
+    responsive:{
+        0:{
+          
+          nav:false,  
+        },
+       768: {
+
+        nav:true,
+       } 
+    }
+})
+
+    // Project Slider
+    $('#project-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    smartSpeed:1000,
+    margin:24,
+    navText:["PREV","NEXT"],
+    responsive:{
+        0:{
+          
+          items:1,
+          nav:false,
+          margin:0,
+            
+        },
+       768: {
+
+        items:2,
+       },
+
+       1140: {
+
+        items:2,
+        center:true,
+        
+       } 
+    }
+});
+
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    items:1,
+    smartSpeed:800,
+  
+})
+
+});
